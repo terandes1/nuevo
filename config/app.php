767f2abf001bfd69ca5cd491f53beb7c3937f014
michael <michael101136@gmail.com>
2018-11-26 09:36:35 -0500

    esfsd
    sdf

--- config/app.php
+++ config/app.php
@@ -159,7 +159,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Greggilbert\Recaptcha\RecaptchaServiceProvider::class,
         /*
          * Package Service Providers...
          */
--- config/captcha.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'secret' => env('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'),
-    'sitekey' => env('6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'),
-    'options' => [
-        'timeout' => 30,
-    ],
-];
